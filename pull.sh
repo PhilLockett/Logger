c19ddd3cb9c0f3d000034853e2472df5efae3e7b
Phil Lockett <phillockett65@gmail.com>
2020-06-02 21:33:28 -0400

    Removed unnecessary scripts.

--- pull.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -u ../Sudoku/Log_c.cpp .
-cp -u ../Sudoku/Log_c.h .
-cp -u ../Sudoku/Logger_c.cpp .
-cp -u ../Sudoku/Logger_c.h .
--- push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -u Log_c.cpp ../Sudoku/
-cp -u Log_c.h ../Sudoku/
-cp -u Logger_c.cpp ../Sudoku/
-cp -u Logger_c.h ../Sudoku/
